6b5af5e141315d24485ef522453ee3c6e4d15db0
math-philip <theusphilipbh@gmail.com>
2024-07-28 20:34:45 -0300

    Update final-script.js

--- final-script.js
+++ final-script.js
@@ -1,25 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const finalImageSrc = localStorage.getItem('finalImage');
     if (finalImageSrc) {
-        const img = document.getElementById('finalImage');
-        img.src = finalImageSrc;
-
-        const downloadButton = document.createElement('button');
-        downloadButton.textContent = 'Baixar foto';
-        downloadButton.onclick = function() {
-            downloadImage(finalImageSrc);
-        };
-
-        document.body.appendChild(downloadButton);
+        const finalImage = document.getElementById('finalImage');
+        finalImage.src = finalImageSrc;
+        const downloadLink = document.getElementById('downloadLink');
+        downloadLink.href = finalImageSrc;
     } else {
-        // Caso não haja imagem final, redireciona de volta para a página de upload
-        window.location.href = 'upload.html';
+        window.location.href = 'index.html';
     }
 });
-
-function downloadImage(imageSrc) {
-    const link = document.createElement('a');
-    link.href = imageSrc;
-    link.download = 'foto-moldura.png';
-    link.click();
-}
